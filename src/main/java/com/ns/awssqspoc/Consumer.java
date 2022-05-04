878d80ac0635f4b794b5bfdb2909da6b26bf149e
animesh.gaur <ani8222@gmail.com>
2022-05-04 16:01:00 +0530

    init

--- src/main/java/com/ns/awssqspoc/Consumer.java
+++ src/main/java/com/ns/awssqspoc/Consumer.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -25,8 +26,8 @@ public class Consumer {
     @SqsListener(value = "https://sqs.ap-south-1.amazonaws.com/325325683952/PRIMARY_SCORING", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void receiveMessage(String stringJson) {
 
-        String message = queueMessagingTemplate.receiveAndConvert(String.class);
-        log.error("Message Received using SQS Listner " + stringJson + " message " + message);
+        Message<?> message = queueMessagingTemplate.receive("PRIMARY_SCORING");
+        log.error("Message Received using SQS Listner " + message.getPayload());
 
     }
 }
