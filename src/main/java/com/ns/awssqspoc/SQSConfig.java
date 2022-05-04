3720e2b5523006028af448114b29555b506114a0
animesh.gaur <ani8222@gmail.com>
2022-05-04 15:39:15 +0530

    init

--- src/main/java/com/ns/awssqspoc/SQSConfig.java
+++ src/main/java/com/ns/awssqspoc/SQSConfig.java
@@ -8,6 +8,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
